f120f358e597fcd22da0ad34b2351cd08b490749
parthsinh-github <parthsinghrajavat95@gmail.com>
2024-09-03 00:27:56 -0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Voting System - Dashboard</title>
+</head>
+<body>
+    <button>Back</button>
+    <button>Log out</button>
+    <h1>Online Voting System</h1>
+    <hr>
+    <div class="Profile"></div>
+    <div class="Group"></div>
+</body>
+</html>
